2551a0a20ac7cfcdd16709f5b6b44a28e29f4dd1
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-11-02 18:57:51 +0100

    add link social to menu

--- src/app/components/about/AboutComponent.tsx
+++ src/app/components/about/AboutComponent.tsx
@@ -3,8 +3,8 @@ import Diplomas from "../diplomas/Diplomas";
 function AboutComponent (){
     return (
         <section className="sm:pt-[15vh] flex p-4 flex-col items-center">
-            <div className="flex flex-col gap-4 items-center w-[75%] sm:w-full sm:items-start">
-                <h2 className="text-6xl">À propos</h2>
+            <div className="flex flex-col gap-4 w-[700px] sm:w-full sm:items-start">
+                <h2 className="text-6xl self-center">À propos</h2>
                 <p className="text-2xl">Commerçant dans l&apos;âme et accro aux nouvelles technologies/Télépilote de drone professionnel.</p>
             </div>
             <Diplomas/>
--- src/app/components/slider/Slides.tsx
+++ src/app/components/slider/Slides.tsx
@@ -1,13 +1,42 @@
 import { useData } from "../../contexts/data";
 import SliderCard from "./SliderCard";
+import { useState, useRef } from "react";
 
 function Slides() {
   const data = useData();
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (containerRef.current) {
+      setIsDragging(true);
+      setStartX(e.pageX);
+      setScrollLeft(containerRef.current.scrollLeft);
+    }
+  };
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (!isDragging || !containerRef.current) return;
+    const x = e.pageX;
+    const diffX = x - startX;
+    const newScrollLeft = scrollLeft - diffX;
+    containerRef.current.scrollLeft = newScrollLeft;
+  };
   return (
     <>
       {data.projects &&
         <section className="flex flex-col gap-4 max-w-[1500px] w-[65%] bottom-[30%] sm:w-full">
-            <div className="flex snap-mandatory overflow-x-scroll snap-x select-none">
+            <div 
+              className="flex snap-mandatory overflow-x-scroll snap-x select-none cursor-grab"
+              ref={containerRef}
+              onMouseDown={handleMouseDown}
+              onMouseUp={handleMouseUp}
+              onMouseLeave={handleMouseUp}
+              onMouseMove={handleMouseMove}
+            >
               {data.projects.map((project) => {
                 return <SliderCard key={project.id} id={project.id} type={project.type} date={project.date} title={project.title} location={project.location} imageMiniature={project.imageMiniature} imageBackground={project.imageBackground} video={project.video} pictures={project.pictures} description={project.description}/>
               })}
