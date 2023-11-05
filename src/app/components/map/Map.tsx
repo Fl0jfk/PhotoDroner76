a80d1e0249826c2f93797b82e65a07594b482cf5
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-11-05 17:45:25 +0100

    add map to contact page

---
+++ src/app/components/map/Map.tsx
@@ -0,0 +1,22 @@
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { useData } from '../../contexts/data';
+
+export default function Map() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+  });
+  if (!isLoaded) return <div>Loading...</div>;
+  return <MapKit/>;
+  
+  function MapKit(){
+    const center = useMemo(() => ({ lat: 49.625889086, lng: 1.175869516 }), []);
+    const data = useData();
+    
+  return (
+      <GoogleMap zoom={9} center={center} mapContainerClassName="w-[40vw] sm:w-[70vw] h-[450px] rounded-xl sm:mx-auto top-[17vh] md:top-[30vh] xl:left-[50vw] lg:left-[50vw] md:left-[55vw] sm:top-[70vh]" options={{ disableDefaultUI: true }}>
+          {data.profile.enterpriselogo&&<Marker position={center} icon={{url: data.profile.enterpriselogo, scaledSize: new window.google.maps.Size(80, 80),}}/>}
+      </GoogleMap>
+    );
+  }
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,8 +5,8 @@ import { AnimatePresence } from 'framer-motion';
 import Header from '@/app/components/header/Header';
 import Footer from '../components/footer/Footer';
 import ContactComponent from '@/app/components/contact/ContactComponent';
-
 import Head from 'next/head';
+import Map from '../components/map/Map';
 
 export default function Contact() {
   return (
@@ -15,10 +15,12 @@ export default function Contact() {
           <title>Page de contact du site PhotoDroner76</title>
       </Head>
       <DataProvider>
-          <main className={`relative h-full min-h-[100vh] min-w-[100vw] w-full select-none xl:overflow-hidden lg:overflow-hidden md:overflow-hidden text-white antialiased bg-black`}>
+          <main className={`relative h-full min-h-[100vh] sm:min-h-[160vh] min-w-[100vw] w-full select-none xl:overflow-hidden lg:overflow-hidden md:overflow-hidden text-white antialiased bg-black`}>
             <AnimatePresence>
               <Header/>
-              <ContactComponent/>
+                <h1 className='text-6xl sm:text-5xl w-full text-center sm:pt-[10vh] md:pt-[10vh]'>Contactez-nous</h1>
+                <ContactComponent/>
+                <Map/>
               <Footer/>
             </AnimatePresence>
           </main>
